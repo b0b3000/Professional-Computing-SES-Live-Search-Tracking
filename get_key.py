3317e30ca2307e768e8af50ff2927e0ea1720480
Robert <bob.beashel@gmail.com>
2024-10-12 12:59:12 +0800

    get_key local running docs

--- get_key.py
+++ get_key.py
@@ -5,73 +5,67 @@
 - Use the next two functions in this file when hosting the web application on Azure.
 """
 
-# First two functions for testing locally:
+# Highlight all following code and use 'ctrl + /' to uncomment/comment block:
+
+KEY_FILEPATH = "keys.txt"
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+VAULT_NAME = "cits32004keys"
+ACCOUNT_NAME = "cits3200testv1"
+
 
 def get_blob_storage_key():
-    """Fetches Azure storage account key from a text file in this directory.
-    
-    Fetches the key, then sets a connection string, where 'AccountName' is the name of the storage account, and 'AccountKey' 
-    is a valid access key to that account.
 
-    Returns:
-        string (str): Connection string for the storage account.
-    """
-    conn_string = "DefaultEndpointsProtocol=https;AccountName=cits3200testv1;AccountKey=;EndpointSuffix=core.windows.net"#
-    with open("keys.txt") as file:
+    # Retrieves key1 from the text file in this directory.
+    # Sets connection string, where AccountName is the name of the Storage Account, and AccountKey is a valid Access Key to that account.
+    conn_string = f"DefaultEndpointsProtocol=https;AccountName={ACCOUNT_NAME};AccountKey=;EndpointSuffix=core.windows.net"
+    with open(KEY_FILEPATH) as file:
         for line in file:
             if line.rstrip().startswith("key1:"):
+                # Splits the key from after the first occurence of "key1:".
                 key = line.rstrip().split("key1:", 1)[1]
+                # Places the key in the correct position in the middle of connection string.
                 return conn_string[:69] + key + conn_string[69:]
-    return ""
 
 
 def get_db_password():
-    """Returns a static Azure database password."""
-    return "meshtastic2024!"
-
-# Next two functions for hosting web application on Azure (use 'ctrl + /' to uncomment block):
-
-# from azure.identity import DefaultAzureCredential
-# from azure.keyvault.secrets import SecretClient
-
-
+    with open(KEY_FILEPATH) as file:
+        for line in file:
+            if line.rstrip().startswith("password:"):
+                # Splits the key from after the first occurence of "password:".
+                password = line.rstrip().split("password:", 1)[1]
+                # Places the key in the correct position in the middle of connection string.
+                return password
+  
 # def get_blob_storage_key():    
-#     """Fetches the Azure storage connection string from a key vault.
 
-#     Fetches the key from a key vault, then sets a connection string, where 'AccountName' is the name of the storage account, 
-#     and 'AccountKey' is a valid access key to that account.
-
-#     Returns:
-#         string (str): Connection string for the storage account.
-#     """
-#     VAULT_NAME = "cits32004keys"
-#     SECRET_NAME = "BlobStorageConnectionString"
+#     secret_name = "BlobStorageConnectionString"
 #     vault_url = f"https://{VAULT_NAME}.vault.azure.net/"
-#     conn_string = "DefaultEndpointsProtocol=https;AccountName=cits3200testv1;AccountKey=;EndpointSuffix=core.windows.net"
-
-#     credential = DefaultAzureCredential()    # Creates a SecretClient using DefaultAzureCredential.
+#     conn_string = f"DefaultEndpointsProtocol=https;AccountName={ACCOUNT_NAME};AccountKey=;EndpointSuffix=core.windows.net"
+    
+#     # Create a SecretClient using DefaultAzureCredential.
+#     credential = DefaultAzureCredential()
 #     client = SecretClient(vault_url=vault_url, credential=credential)
     
-#     retrieved_secret = client.get_secret(SECRET_NAME)    # Retrieves the secret from the Key Vault.
+#     # Retrieve the secret from the Key Vault
+#     retrieved_secret = client.get_secret(secret_name)
 #     key = retrieved_secret.value.rstrip()
-
+    
+#     #Format correctly
 #     return conn_string[:69] + key + conn_string[69:]
 
 
 # def get_db_password():
-#     """Fetches the Azure database password from a key vault.
-
-#     Returns:
-#         string (str): Azure database password.
-#     """
-#     VAULT_NAME = "cits32004keys"
-#     SECRET_NAME = "historicalDatabasePassword"
+#     secret_name = "historicalDatabasePassword"
 #     vault_url = f"https://{VAULT_NAME}.vault.azure.net/"
-#     # Creates a SecretClient using DefaultAzureCredential.
+    
+#     # Create a SecretClient using DefaultAzureCredential
 #     credential = DefaultAzureCredential()
 #     client = SecretClient(vault_url=vault_url, credential=credential)
-
-#     retrieved_secret = client.get_secret(SECRET_NAME)    # Retrieve the secret from the Key Vault
+    
+#     # Retrieve the secret from the Key Vault
+#     retrieved_secret = client.get_secret(secret_name)
 #     password = retrieved_secret.value.rstrip()
-
+    
 #     return password
+
